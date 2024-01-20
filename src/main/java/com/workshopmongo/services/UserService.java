8253ae24f5d7755acb8ce6c244a26b519e66e9e7
Mauricio Junior <mjunior1205@gmail.com>
2024-01-20 13:51:08 -0300

    User update with PUT

--- src/main/java/com/workshopmongo/services/UserService.java
+++ src/main/java/com/workshopmongo/services/UserService.java
@@ -32,11 +32,21 @@ public class UserService {
 	}
 	
 	public void delete(String id) {
-		
 		findById(id);
 		repository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User  newUser = findById(user.getId());
+		updateData(newUser, user);
+		return repository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
